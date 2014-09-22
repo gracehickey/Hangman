d5f33acfeb8d537fb1c2d410aa76bb527d902479
grace.hickey <grace.hickey@MU-C3-11.mullen.pvt.k12.co.us>
2014-09-22 15:10:57 -0600

    Initial Commit

---
+++ Personal Project/src/Data.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+	 
+	public class Data
+		{
+		static ArrayList <Hangman> data = new ArrayList <Hangman>();
+	 
+		public static void addData()
+			{
+			data.add(new Hangman("a"));
+			data.add(new Hangman("b"));
+			data.add(new Hangman("c"));
+			data.add(new Hangman("d"));
+			data.add(new Hangman("e"));
+			data.add(new Hangman("f"));
+			data.add(new Hangman("g"));
+			data.add(new Hangman("h"));
+			data.add(new Hangman("i"));
+			data.add(new Hangman("j"));
+			data.add(new Hangman("k"));
+			data.add(new Hangman("l"));
+			data.add(new Hangman("m"));
+			data.add(new Hangman("n"));
+			data.add(new Hangman("o"));
+			data.add(new Hangman("p"));
+			data.add(new Hangman("q"));
+			data.add(new Hangman("r"));
+			data.add(new Hangman("s"));
+			data.add(new Hangman("t"));
+			data.add(new Hangman("u"));
+			data.add(new Hangman("v"));
+			data.add(new Hangman("w"));
+			data.add(new Hangman("x"));
+			data.add(new Hangman("y"));
+			data.add(new Hangman("z"));
+			
+			
+			}
+		}
---
+++ Personal Project/src/Hangman.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Hangman
+	{
+		private String name;
+		private String letter;
+		private int attempts;
+	
+public Hangman(String n, String l, int a)
+	{
+	name = n;
+	letter = l;
+	attempts = a;                                                                                                                                                                                                                                                                                                                                
+	}
+public String getName()
+	{
+	return name;
+	}
+public String getLetter()
+	{
+	return letter;
+	}
+public int getAttempts()
+	{
+	return attempts;
+	}
+                                                                                      
+	public static void main(String[] args)
+		{
+		String arrayWord = "a";
+		if(Word.contains = "a")
+			{
+			
+			}
+
+	}
